bbf78bf96bdc19d1318b2e552f87759d217f59b1
danielins <daniel.alves.321@gmail.com>
2016-11-23 18:39:11 -0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/**
+ * Grunt configuration
+ */
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			css: {
+				files: ['css/build/*.css'],
+				tasks: ['concat', 'cssmin']
+			}
+		},
+
+		concat: {
+			dist: {
+				src: ['css/build/base.css', 'css/build/*.css'],
+				dest: 'css/main.css'
+			}
+		},
+
+		cssmin: {
+			target: {
+				files: {
+					'css/main.min.css': 'css/main.css'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.registerTask('default', ['concat', 'cssmin', 'watch']);
+
+};
